fcb2266ed3c4e8d5da2c01efd3dbb64727bdf5e4
Ubuntu <ubuntu@ip-172-31-91-0.ec2.internal>
2019-11-05 10:24:31 +0000

    user login & link to issue tracker user details

--- app1_home/views.py
+++ app1_home/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
+from django.contrib import messages
+from accounts.forms import UserLoginForm
+from app2_user_details.models import UserDetail
 
 # Home Page
 def home(request):
     
     """ A view that renders the index page """
     
-    return render(request, "home.html")
+    if request.user.is_authenticated: 
+        login_form = UserLoginForm(request.POST)
+        
+        # Check that the user has been set up for the Issue Tracking System
+    
+        UserDetails = ""
+        try:
+            UserDetails = UserDetail.objects.get(user_name=request.user.username)
+            messages.success(request, "You are already logged in!")
+            return render(request, 'userpage.html', {'userdetails': UserDetails })
+        except:
+            login_form.add_error(None, "User not set up on Issue Tracking System")
+        
+    else:
+        return render(request, "home.html")
     
