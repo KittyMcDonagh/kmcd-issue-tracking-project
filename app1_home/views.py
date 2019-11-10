ef924c33aa7be7980669f64a46dad2bda45da3bf
Ubuntu <ubuntu@ip-172-31-91-0.ec2.internal>
2019-11-10 11:16:26 +0000

    displaying vendor/customer name top of screen

--- app1_home/views.py
+++ app1_home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.contrib import messages
 from accounts.forms import UserLoginForm
 from app2_user_home.models import UserDetail
+from app2_user_home.models import Vendor
+from app2_user_home.models import Customer
+
 
 # Home Page
 def home(request):
@@ -13,15 +16,32 @@ def home(request):
     if request.user.is_authenticated: 
         login_form = UserLoginForm(request.POST)
     
-    # User already logged in. Get the user details from the Issue Tracking System
+        # User already logged in. Get the user details from the Issue Tracking System
     
-        try:
-            UserDetails = UserDetail.objects.get(user_name=request.user.username)
-            messages.success(request, "You are already logged in!")
-            return render(request, 'home.html', {'userdetails': UserDetails })
-        except:
-            login_form.add_error(None, "User not set up on Issue Tracking System")
+        UserDetails = UserDetail.objects.get(user_name=request.user.username)
+        messages.success(request, "You are already logged in!")
+    
+        # Get the Vendor or Customer Details depending on which the user is 
+        # associated with
         
+        CustomerDetails = ""
+        VendorDetails = ""
+    
+        if UserDetails.user_type == 'C':
+        
+            try:
+                CustomerDetails = Customer.objects.get(cust_code=UserDetails.vend_cust_code)
+            except:
+                messages.success(request, "Customer details not found!")
+    
+        else:
+            try:
+                VendorDetails = Vendor.objects.get(vend_code=UserDetails.vend_cust_code)
+            except:
+                messages.success(request, "Vendor details not found!")
+    
+        return render(request, 'home.html', {'userdetails': UserDetails, 'customerdetails': CustomerDetails, 'vendordetails': VendorDetails })
+    
     else:
         return render(request, "home.html")
     
