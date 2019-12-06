44fb291d76629599b7b2ea6b9f15fc7da7f6e164
Ubuntu <ubuntu@ip-172-31-91-0.ec2.internal>
2019-12-06 18:55:43 +0000

    diferentiating issues and features

--- app4_features/urls.py
+++ app4_features/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import features_home, feature_details, all_features_list, get_features, new_edit_feature, update_feature_status
+from .views import features_home, feature_details, get_features, new_edit_feature, update_feature_status
 
 urlpatterns = [
     url(r'^features/$', features_home, name="features_home"),
@@ -8,7 +8,6 @@ urlpatterns = [
     url(r'^features/(?P<pk>\d+)/edit/$', new_edit_feature, name='edit_feature'),
     url(r'^features/(?P<pk>\d+)/update/$', update_feature_status, name='update_feature_status'),
     url(r'^features/(?P<pk>\d+)/$', feature_details, name='feature_details'),
-    url(r'^features/all$', all_features_list, name="all_features_list"),
     url(r'^features/featurelist$', get_features, name="get_features"),
    
 ]
