a1a779ec8bb920177002c3a2be9122e4c60d9aa8
Ubuntu <ubuntu@ip-172-31-91-0.ec2.internal>
2019-12-10 12:27:14 +0000

    js adding to cart

--- app6_cart/urls.py
+++ app6_cart/urls.py
@@ -1,11 +1,12 @@
 # Code copied from the e-commerce lesson
 
 from django.conf.urls import url
-from .views import view_cart, add_to_cart, adjust_cart
+from .views import view_cart, add_to_cart, add_to_cart_js, adjust_cart
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
     url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
+    url(r'^add_js/', add_to_cart_js, name='add_to_cart_js'),
     url(r'^adjust/(?P<id>\d+)', adjust_cart, name='adjust_cart'),
     
 ]
--- app6_cart/views.py
+++ app6_cart/views.py
@@ -1,6 +1,8 @@
 # Initial code copied from the e-commerce project
 
 from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse
+from django.http import JsonResponse 
 
 
 """
@@ -16,25 +18,23 @@ def view_cart(request):
     
 
 """
-Add a quantity of the specified product to the cart
+Add a quantity of the specified feature id to the cart
 """ 
 
 def add_to_cart(request, id):
     
-    print("in add_to_cart -------------------------------------")
+    print("in add_to_cart, id -------------------------------------")
         
     # This takes an integer from the form we created
     
-    print(request.method)
-    print("id = "+str(id))
+    print("request method: "+str(request.method))
+    print("id: "+str(id))
     
-    if request.method == 'POST':
-        quantity = int(request.POST.get('quantity'))
-    else:
-        quantity = (request.GET.get('quantity'))
+    # Get the quantity we specified
+    
+    quantity = int(request.POST.get('quantity'))
     
     print("quantity: "+str(quantity))
-   
     
     # This is going to the cart from the session (not from the database), 
     # and it gets a cart if one already exists, otherwise it gets an empty
@@ -51,12 +51,60 @@ def add_to_cart(request, id):
         
     else:
         cart[id] = cart.get(id, quantity)
-    
         
     request.session['cart'] = cart
         
     return redirect(reverse('features_home'))
         
+
+# This view is called via the js (".add-btn-click") in base.html
+
+def add_to_cart_js(request):
+    
+    print("in add_to_cart_js -------------------------------------")
+        
+    # This takes an integer from the form we created
+    
+    print("request method: "+str(request.method))
+    
+    id = int(request.POST.get('featureId'))
+    quantity = int(request.POST.get('qty'))
+    
+    print("id: "+str(id))
+    print("quantity: "+str(quantity))
+    
+    # This is going to get the cart from the session (not from the database), 
+    # and it gets a cart if one already exists, otherwise it gets an empty
+    # dictionary
+    
+    print("about to get cart_js----------------------------------------")
+    cart = request.session.get('cart', {})
+    print("have cart_js----------------------------------------")
+        
+    # Add cart id and quantity
+    
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity
+        
+    else:
+        cart[id] = cart.get(id, quantity)
+    
+    # Update the cart
+        
+    request.session['cart'] = cart
+    
+    
+    data = {}
+    
+    data["cart"] = {
+        
+        "qty":quantity
+        
+        }
+    
+    return JsonResponse(data, safe=False)
+
+
 
 """
 Adjust the quantity of the specified feature to the specified amount
