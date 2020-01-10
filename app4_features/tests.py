b9db7f20f76ff561b88e6bcbe8cd603a96794b16
Ubuntu <ubuntu@ip-172-31-91-0.ec2.internal>
2020-01-10 12:17:15 +0000

    features test removed

--- app4_features/tests.py
+++ app4_features/tests.py
@@ -1,14 +1,4 @@
 from django.test import TestCase
-from .models import UserDetail
 
-# Test UserDetails.
+# Create your tests here.
 
-class UserDetailsTests(TestCase):
-    
-    """
-    Here we'll define the tests that we'll run against our UserDetails model
-    """
-    
-    def test_str(self):
-        test_first_name=UserDetail(user_first_name='Kitty')
-        self.assertEqual(str(test_first_name), 'Kitty')
